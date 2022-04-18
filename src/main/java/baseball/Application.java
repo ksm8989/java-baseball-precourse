bf0c510dc76008cb3c09107d1d6ba8bbee0d34cf
Sangmoon <sm89star@gmail.com>
2022-04-18 21:33:17 +0900

    feat: 게임 실행을 위한  메인 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
+import baseball.controller.BaseballController;
+import camp.nextstep.edu.missionutils.Console;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballController baseballController = new BaseballController();
+        baseballController.start();
     }
+
 }
