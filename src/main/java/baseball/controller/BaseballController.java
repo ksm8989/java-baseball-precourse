a3e8afb699bd7c7502d7b1e06a3f6ec6c9a70a23
Sangmoon <sm89star@gmail.com>
2022-04-18 21:30:54 +0900

    feat: 게임 컨트롤러

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,57 @@
+package baseball.controller;
+
+import baseball.service.BaseballService;
+import baseball.service.NumberService;
+import baseball.view.BaseballConsoleView;
+import baseball.utils.BaseballUtils;
+import baseball.constant.CodeConst;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BaseballController {
+
+    private ArrayList computerNumberList;
+    private String exitCode;  // "1" -> start ,  "2" -> end
+
+    public BaseballController() {
+        generateComputerNumberList();
+        this.exitCode = CodeConst.START_CODE;
+    }
+
+    private void generateComputerNumberList(){
+        this.computerNumberList = NumberService.generateNumber();
+    }
+
+    public void start() {
+        while (CodeConst.START_CODE.equals(exitCode)) {
+            String inputNumber = BaseballConsoleView.inputNumber();
+            validateInput(inputNumber);
+            ArrayList inputList = BaseballUtils.convertStringToArray(inputNumber);
+
+            if(BaseballService.compareInputNumberAndComputerNumber(inputList, this.computerNumberList)){
+                gameEnd();
+                restart();
+            }
+        }
+    }
+
+    private void restart() {
+        exitCode = BaseballConsoleView.restartGame();
+        //ValidateService.validateSelectionRange(isExit);
+        if (CodeConst.START_CODE.equals(exitCode)) {
+            generateComputerNumberList();
+        }
+    }
+
+    private void gameEnd() {
+        BaseballConsoleView.endGame();
+    }
+
+    private void validateInput(String input) {
+        NumberService.validateIsNumber(input);
+        NumberService.validateNumberRange(input);
+        NumberService.validateLength(input);
+    }
+}
